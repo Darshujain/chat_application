b41c87321249468ebe6359494ade02365dadb362
Darshana jain <69414299+Darshujain@users.noreply.github.com>
2025-08-18 15:03:14 +0530

    Update Chat.js

--- project_chat_application-master/project_chat_application-master/client/src/components/Chat/Chat.js
+++ project_chat_application-master/project_chat_application-master/client/src/components/Chat/Chat.js
@@ -9,64 +9,72 @@ import Input from '../Input/Input';
 
 import './Chat.css';
 
-const ENDPOINT =  'http://localhost:5000';
+const ENDPOINT = 'http://localhost:5000';
 
 let socket;
 
 const Chat = ({ location }) => {
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
-  const [users, setUsers] = useState('');
+  const [users, setUsers] = useState([]);
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
 
+  // Join room
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
 
     socket = io(ENDPOINT);
 
+    setName(name);
     setRoom(room);
-    setName(name)
 
     socket.emit('join', { name, room }, (error) => {
-      if(error) {
-        alert(error);
-      }
+      if (error) alert(error);
     });
+
+    // Cleanup to prevent duplicate connections
+    return () => {
+      socket.disconnect();
+      socket.off();
+    };
   }, [location.search]);
-  
+
+  // Listeners for messages and room data
   useEffect(() => {
-    socket.on('message', message => {
-      setMessages(messages => [ ...messages, message ]);
-    });
-    
-    socket.on("roomData", ({ users }) => {
+    const messageHandler = (message) => {
+      setMessages(prevMessages => [...prevMessages, message]);
+    };
+
+    const roomDataHandler = ({ users }) => {
       setUsers(users);
-    });
-}, []);
+    };
+
+    socket.on('message', messageHandler);
+    socket.on('roomData', roomDataHandler);
+
+    // Cleanup listeners to prevent duplicates
+    return () => {
+      socket.off('message', messageHandler);
+      socket.off('roomData', roomDataHandler);
+    };
+  }, []);
 
   const sendMessage = (event) => {
-  event.preventDefault();
-  console.log('Send button clicked. Message:', message);
-
-  if (message) {
-    socket.emit('sendMessage', message, () => {
-      console.log('Message sent, clearing input');
-      setMessage('');
-    });
-  } else {
-    console.log('Message is empty, not sending');
+    event.preventDefault();
+    if (message) {
+      socket.emit('sendMessage', message, () => setMessage(''));
+    }
   }
-}
 
   return (
     <div className="outerContainer">
       <div className="container">
-          <InfoBar room={room} />
-          <Messages messages={messages} name={name} />
-          <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+        <InfoBar room={room} />
+        <Messages messages={messages} name={name} />
+        <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
       </div>
-      <TextContainer users={users}/>
+      <TextContainer users={users} />
     </div>
   );
 }
